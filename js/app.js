6a0f5fa37254e10efd895293659efffd43d80be3
seokenny <kenn.seo94@gmail.com>
2019-08-17 19:59:44 -0400

    Menu and lcations

--- js/app.js
+++ js/app.js
@@ -5,6 +5,32 @@ $(window).scroll(function(){
     }
 });
 
-$(".hsq_0").on("click",function(){
-    window.history.pushState(null, null, "/testing.html");
+// $(".hsq_0").on("click",function(){
+//     window.history.pushState(null, null, "/testing.html");
+//     $(".white_overlay").removeClass("hidden");
+//     $(".white_overlay").addClass("fadeIn",function(){
+//         $(".white_overlay").removeClass("fadeIn");
+//         $(".white_overlay").addClass("fadeOut");
+//     });
+// });
+
+$(window).scroll(function(){
+    if($(window).scrollTop()>300){
+        $(".to_top").removeClass("hidden");
+       $(".to_top").addClass("fadeIn");
+    }
+});
+
+$(".hsq_h1").on("click",function(){
+    window.history.pushState(null, null, "/locations.html");
+    $(".white_overlay").addClass("fadeIn",function(){
+        $(".white_overlay").delay(1000).removeClass("fadeIn",function(){
+            $(".white_overlay").addClass("fadeOut");
+        });
+    });
+    $("#container").addClass("hidden");
+    $("#container").addClass("fadeOut");
+    $("#container_locations").removeClass("hidden");
+    $("#container_locations").addClass("fadeIn");
+
 });
